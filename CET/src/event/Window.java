c198efa7e0508dd4578bc1ecc25a2fd8b14ab44d
olgapoppe <olga.poppe@gmail.com>
2016-02-09 16:02:46 -0500

    Input can be generated by batch or by window.

--- CET/src/event/Window.java
+++ CET/src/event/Window.java
@@ -7,11 +7,13 @@ public class Window {
 	public int start;
 	public int end;
 	public ArrayList<Event> events;
+	public int event_number;
 		
 	public Window (int s, int e) {
 		start = s;
 		end = e;
 		events = new ArrayList<Event>();
+		event_number = 0;
 	}
 	
 	public boolean relevant (Event e) {
--- CET/src/iogenerator/InputFileGenerator.java
+++ CET/src/iogenerator/InputFileGenerator.java
@@ -11,105 +11,178 @@ import event.*;
 
 public class InputFileGenerator {
 	
-	/**
-	 * INPUT PARAMETERS:
-	 * @param args
-	 * 0 output file CET/src/iofiles/stream.txt
-	 * 1 max_time_progress 30
-	 * 2 max_comp 2
-	 * 3 last minute 5
-	 */	
-	public static void main (String [] args) {
+	public static void main (String[] args) {
 		
-		try { 		
-			// Open the output file
-			String output_file_name = "CET\\src\\iofiles\\rate9.txt"; 
-			File output_file = new File(output_file_name);
-			BufferedWriter output;
-			output = new BufferedWriter(new FileWriter(output_file)); 
-			
-			// Read input parameters
-			int max_time_progress = 150; 
-			int max_comp = 3;  
-			int last_min = 30;
-			int rate_limit = 200;
+		try {
+		// Open the output file
+		String output_file_name = "CET\\src\\iofiles\\rate9.txt"; 
+		File output_file = new File(output_file_name);
+		BufferedWriter output = new BufferedWriter(new FileWriter(output_file));
 		
-			// Local variables
-			Random random = new Random();
-			int batch_size = 300; 
-			int last_sec = last_min * 60; 
-			int comp = 0;	
-			
-			int event_id = 0;
-			int value = 1; 
-			
-			int sequence_number = 0;
-			int max_event_rate = 0;
-			
-			// Output the parameters
-			System.out.println(
-					"Stream length: " + last_min + " min" +
-					"\nMax time progress: " + max_time_progress +
-					"\nMax compatibility: " + max_comp +
-					"\nEvent rate limit: " + rate_limit +
-					"\n---------------------");
-			
-			// Generate sequences in batches
-			ArrayList<ArrayDeque<Event>> all_events = new ArrayList<ArrayDeque<Event>> ();
-			
-			for (int iteration_number=0; iteration_number<last_sec/batch_size; iteration_number++) { 
+		// Generate the input event stream
+		//generate_batches(output);
+		generate_windows(output);
+		
+		// Close the file
+		output.close();	
+		
+		} catch (IOException e) { e.printStackTrace(); }
+	}
+	
+	public static void generate_windows (BufferedWriter output) {
+		
+		// Read input parameters
+		int last_sec = 1800;
+		int window_length = 600;
+		int window_slide = 300;
+		int event_number_per_window = 10;
+		int max_comp = 3;
+		
+		// Local variables
+		double window_number = Math.ceil((double)last_sec / (double)window_slide);
 				
-				int sec = 0;
-				int offset = iteration_number*batch_size;
-				int event_number = 0;
-				all_events.clear();
+		// Output the parameters
+		System.out.println(
+				"Stream length: " + last_sec + " sec" +
+				"\nWindow length: " + window_length +
+				"\nWindow slide: " + window_slide +
+				"\nWindow number: " + window_number +
+				"\nEvent number per window: " + event_number_per_window +
+				"\nMax compatibility: " + max_comp +
+				"\n---------------------");
+		
+		// Generate windows
+		ArrayList<Window> windows = new ArrayList<Window>();
+		for (int window_id=0; window_id<window_number; window_id++) {			
+			int start = window_id * window_slide;
+			int end = start + window_length;
+			Window window = new Window(start,end);
+			windows.add(window);
+		}
+		
+		// Generate events and keep track of their number per window
+		int event_id = 1;
+		for (Window window : windows) {			
+			ArrayList<Event> events = getEvents(window, event_id, event_number_per_window, max_comp);
+			for (Event event : events) {
+				try { output.append(event.print2file()); } catch (IOException e) { e.printStackTrace(); }
+			}
+			event_id += event_number_per_window;
+		}	
+	}	
+	
+	public static ArrayList<Event> getEvents(Window window, int event_id, int event_number_per_window, int max_comp) {
+		
+		ArrayList<Event> events = new ArrayList<Event>();		
+				
+		// First event
+		int sec = window.start;
+		Event e1 = new Event(sec,event_id,1);
+		events.add(e1);
+		event_id++;
+		int event_number = 1;
+							
+		Random random = new Random();
+		
+		while (event_number<event_number_per_window && sec<=window.end) {
+			
+			// Random time progress 
+			sec++;
+			// Random event compatibility
+			int comp = random.nextInt(max_comp + 1) + 1;
+			if (event_number+comp>event_number_per_window) comp = event_number_per_window - event_number;
+			// Following events
+			for (int i=1; i<=comp; i++) {
+				Event e2 = new Event(sec,event_id,1);
+				events.add(e2);
+				event_id++;
+				event_number++;
+			}
+		}
+		
+		
+		return events;		
+	}
+	
+	public static void generate_batches (BufferedWriter output) {
+		
+		// Read input parameters
+		int max_time_progress = 150; 
+		int max_comp = 3;  
+		int last_min = 30;
+		int rate_limit = 200;
+		
+		// Local variables
+		Random random = new Random();
+		int batch_size = 300; 
+		int last_sec = last_min * 60; 
+		int comp = 0;	
+			
+		int event_id = 0;
+		int value = 1; 
+			
+		int sequence_number = 0;
+		int max_event_rate = 0;
+			
+		// Output the parameters
+		System.out.println(
+				"Stream length: " + last_min + " min" +
+				"\nMax time progress: " + max_time_progress +
+				"\nMax compatibility: " + max_comp +
+				"\nEvent rate limit: " + rate_limit +
+				"\n---------------------");
+			
+		// Generate sequences in batches
+		ArrayList<ArrayDeque<Event>> all_events = new ArrayList<ArrayDeque<Event>> ();
+			
+		for (int iteration_number=0; iteration_number<last_sec/batch_size; iteration_number++) { 
+				
+			int sec = 0;
+			int offset = iteration_number*batch_size;
+			int event_number = 0;
+			all_events.clear();
 						
-				System.out.println("Iteration " + iteration_number);
+			System.out.println("Iteration " + iteration_number);
 			
-				while (sec<=batch_size) {			
-				 
-					ArrayDeque<Event> events_with_same_value = new ArrayDeque<Event> ();
-					int curr_sequence_number = 1;
+			while (sec<=batch_size) {			
+			 
+				ArrayDeque<Event> events_with_same_value = new ArrayDeque<Event> ();
+				int curr_sequence_number = 1;
 			
-					// First event in a sequence
-					sec = random.nextInt(max_time_progress);
-					Event e1 = new Event(sec+offset,event_id,value);	
-					events_with_same_value.add(e1);				
-					event_id++;
+				// First event in a sequence
+				sec = random.nextInt(max_time_progress);
+				Event e1 = new Event(sec+offset,event_id,value);	
+				events_with_same_value.add(e1);				
+				event_id++;
+				comp = random.nextInt(max_comp + 1) + 1;
+				if (comp>1) curr_sequence_number *= comp;
+			
+				// All following events
+				while (comp>0) {
+					sec = sec + random.nextInt(max_time_progress) + 1;
+					if (sec>batch_size) break;
+					for (int i=0; i<comp; i++) {
+						Event e2 = new Event(sec+offset,event_id,value);
+						events_with_same_value.add(e2);
+						event_id++;
+					}
 					comp = random.nextInt(max_comp + 1) + 1;
 					if (comp>1) curr_sequence_number *= comp;
-			
-					// All following events
-					while (comp>0) {
-						sec = sec + random.nextInt(max_time_progress) + 1;
-						if (sec>batch_size) break;
-						for (int i=0; i<comp; i++) {
-							Event e2 = new Event(sec+offset,event_id,value);
-							events_with_same_value.add(e2);
-							event_id++;
-						}
-						comp = random.nextInt(max_comp + 1) + 1;
-						if (comp>1) curr_sequence_number *= comp;
-					}	
-					all_events.add(events_with_same_value);
-					event_number += events_with_same_value.size();
-					sequence_number += curr_sequence_number;
-					System.out.println(" Value " + value + " graph size " + events_with_same_value.size());
-				}
-				// Put events in the file in order by time stamp
-				int event_rate = write2File(all_events,output,event_number,rate_limit);	
-				if (max_event_rate<event_rate) max_event_rate = event_rate;				
-		    }
-			
-			// Close the file
-			output.close();	
-			
-			System.out.println("---------------------" + 
-					"\nSequence number: " + sequence_number +
-					"\nEvent number: " + event_id +
-					"\nMax event rate: " + max_event_rate);	
-			
-		} catch (IOException e) { e.printStackTrace(); }
+				}	
+				all_events.add(events_with_same_value);
+				event_number += events_with_same_value.size();
+				sequence_number += curr_sequence_number;
+				System.out.println(" Value " + value + " graph size " + events_with_same_value.size());
+			}
+			// Put events in the file in order by time stamp
+			int event_rate = write2File(all_events,output,event_number,rate_limit);	
+			if (max_event_rate<event_rate) max_event_rate = event_rate;				
+		}
+		// Output statistics
+		System.out.println("---------------------" + 
+				"\nSequence number: " + sequence_number +
+				"\nEvent number: " + event_id +
+				"\nMax event rate: " + max_event_rate);	
 	}
 	
 	public static int write2File(ArrayList<ArrayDeque<Event>> all_events, BufferedWriter output, int event_number, int rate_limit) {
