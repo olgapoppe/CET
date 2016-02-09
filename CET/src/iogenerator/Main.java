47a6fbe47ff9adda13c0fa255880c3db6cb92452
olgapoppe <olga.poppe@gmail.com>
2016-02-08 22:06:25 -0500

    Path fixed.

--- CET/src/iogenerator/Main.java
+++ CET/src/iogenerator/Main.java
@@ -30,7 +30,7 @@ public class Main {
 	    
 	    /*** Input and output ***/
 	    // Set default values
-	    String path = "src/iofiles/";
+	    String path = "iofiles/";
 		String inputfile = "stream1.txt";
 		String outputfile = "sequences.txt";
 		OutputFileGenerator output = new OutputFileGenerator(path+outputfile); 
