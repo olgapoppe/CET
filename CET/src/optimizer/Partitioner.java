262a9ac8acc4ba0be3310cf22be75e859ff64265
opoppe <olga.poppe@gmail.com>
2016-02-17 13:54:48 -0500

    Added Partitioner interface with 3 classes implementing it for different search space traversal algorithms.

--- CET/src/optimizer/Partitioner.java
+++ CET/src/optimizer/Partitioner.java
@@ -1,39 +1,7 @@
 package optimizer;
 
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import graph.*;
-
-public class Partitioner {
+public interface Partitioner {
 	
-	boolean pruning;
+	public Partitioning getPartitioning (Partitioning root, int m);
 	
-	Partitioner (boolean p) {
-		pruning = p;
-	}
-	
-	public Partitioning getPartitioning (Partitioning root, int m) {
-		
-		Partitioning solution = new Partitioning(new ArrayList<Partition>());
-		
-		double minCPU = Integer.MAX_VALUE;
-		
-		ArrayDeque<Partitioning> heap = new ArrayDeque<Partitioning>();
-		heap.add(root);
-		
-		while (!heap.isEmpty()) {
-			Partitioning temp = heap.poll();
-			double temp_cpu = temp.getCPUcost();
-			if (minCPU > temp_cpu) {
-				minCPU = temp_cpu;
-				solution = temp;
-			}
-			ArrayList<Partitioning> children = temp.getChildren();
-			for (Partitioning child : children) {
-				if (!pruning || child.getMEMcost() <= m) heap.push(child);
-			}
-		}		
-		return solution;		
-	}
-
 }
