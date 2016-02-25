d15e40d2a1c5ea4a66697366e9e92f0f8ffeb0f8
olgapoppe <olga.poppe@gmail.com>
2016-02-25 13:05:17 -0500

    Input files on compatibility are updated.

--- CET/src/iogenerator/InputFileGenerator.java
+++ CET/src/iogenerator/InputFileGenerator.java
@@ -17,9 +17,9 @@ public class InputFileGenerator {
 		
 		// Parameters to vary
 		int event_number_per_window = 20;
-		int max_comp = 7;
+		int max_comp = 15;
 		int window_length = 310;
-		int window_slide = 300;
+		int window_slide = 311;
 		
 		try {
 		// Open the output file
@@ -42,7 +42,7 @@ public class InputFileGenerator {
 	public static void generate_windows (BufferedWriter output, int event_number_per_window, int max_comp, int window_length, int window_slide) {
 		
 		// Local variables
-		int last_sec = 1800;
+		int last_sec = 310;
 		double window_number = Math.ceil((double)last_sec / (double)window_slide);
 				
 		// Output the parameters
