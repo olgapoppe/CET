af6b62d7721cb0cba0838f90faa4faf4b7d24aa1
olgapoppe <olga.poppe@gmail.com>
2016-03-28 16:21:08 -0400

    Testing event compatibility 2.

--- CET/src/iogenerator/InputFileGenerator.java
+++ CET/src/iogenerator/InputFileGenerator.java
@@ -19,8 +19,8 @@ public class InputFileGenerator {
 		int action = Integer.parseInt(args[0]);
 		
 		// Parameters to vary
-		int event_number_per_window = 25;
-		int max_comp = 3;
+		int event_number_per_window = 20;
+		int max_comp = 2;
 		int window_length = 310;
 		int window_slide = 311;
 		int time_progress = 30;
@@ -32,8 +32,8 @@ public class InputFileGenerator {
 		if (action == 0) {		
 			
 			// Open the output file
-			String output_file_name = "CET\\src\\iofiles\\rate" + event_number_per_window + ".txt"; 
-			//String output_file_name = "CET\\src\\iofiles\\comp" + max_comp + ".txt";
+			//String output_file_name = "CET\\src\\iofiles\\rate" + event_number_per_window + ".txt"; 
+			String output_file_name = "CET\\src\\iofiles\\comp" + max_comp + ".txt";
 			//String output_file_name = "CET\\src\\iofiles\\stream.txt";
 			File output_file = new File(output_file_name);
 			BufferedWriter output = new BufferedWriter(new FileWriter(output_file));
